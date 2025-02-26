6b40c46fa453887dcd63b937d45fc7548323585a
Dahlia <embracedahlia@gmail.com>
2025-02-25 21:59:00 -0600

    revised Prog6 for edge case and performance

--- lab1/Prog6.java
+++ lab1/Prog6.java
@@ -2,21 +2,26 @@ package lab1;
 
 public class Prog6 {
 
-    public static void secondMin(int[] arrayOfInts){
+    public static void secondMin(int[] arrayOfInts) {
+        if (arrayOfInts == null || arrayOfInts.length < 2) {
+            System.out.println("The array should have at least 2 values to find the second minimum!");
+            return;
+        }
         int min = Integer.MAX_VALUE;
-        int secondMin = min-1;
-        for (int i=0;i<arrayOfInts.length;i++) {
-            if (arrayOfInts[i] < secondMin && arrayOfInts[i] < min) {
-                if (min == Integer.MAX_VALUE)
-                    secondMin = arrayOfInts[i];
-                else
-                    secondMin = min;
+        int secondMin = Integer.MAX_VALUE;
+        for (int i = 0; i < arrayOfInts.length; i++) {
+            if (arrayOfInts[i] < min) {
+                secondMin = min;
                 min = arrayOfInts[i];
             } else if (arrayOfInts[i] < secondMin && arrayOfInts[i] != min) {
                 secondMin = arrayOfInts[i];
             }
         }
-        System.out.println("The second minimum value is "+secondMin);
+        if (secondMin == Integer.MAX_VALUE) {
+            System.out.println("No second minimum value!");
+        } else {
+            System.out.println("The second minimum value is " + secondMin + ".");
+        }
     }
 
     public static void main(String[] args) {
