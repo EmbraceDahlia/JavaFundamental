c71889978e973d8f87b485f315fde0de06fab081
Gideon Bamuleseyo <gideonbamuleseyo@gmail.com>
2025-03-01 20:40:07 -0600

    Format code

--- lab3/MaxValue.java
+++ lab3/MaxValue.java
@@ -2,31 +2,21 @@ package lab3;
 
 public class MaxValue {
 
-    private static int findMaxValue(int[] a,int index){
-        if(index == a.length-1){
+    private static int findMaxValue(int[] a, int index) {
+        if (index == a.length - 1) {
             return a[index];
         }
+        return ((a[index] > findMaxValue(a, index + 1) ? a[index] : findMaxValue(a, index + 1)));
+    }
 
-        return ( (a[index]> findMaxValue(a, index + 1)?
-        a[index]:findMaxValue(a, index + 1)));
-
-    } 
-
-    public static int findMaxValue(int[]a){
-        if(a.length==0){
+    public static int findMaxValue(int[] a) {
+        if (a.length == 0) {
             return 0;
         }
-
         return findMaxValue(a, 0);
-
     }
 
-    public static void main(String[] args){
-       System.out.println(findMaxValue(new int[]{}));
+    public static void main(String[] args) {
+        System.out.println(findMaxValue(new int[] {}));
     }
-
-    
-    
-
-    
 }
