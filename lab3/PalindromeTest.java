39ff2d7724baf30500601cff1ab548334390ad1d
Gideon Bamuleseyo <gideonbamuleseyo@gmail.com>
2025-03-01 21:31:53 -0600

    Add readme to lab3

--- lab3/PalindromeTest.java
+++ lab3/PalindromeTest.java
@@ -8,30 +8,26 @@ class PalindromeTest {
     @Test
     void testPalindromeNumbers() {
         assertTrue(Palindrome.isPalindromeNumber(121), "121 is a palindrome");
-        assertTrue(Palindrome.isPalindromeNumber(44),"44 is a palindrome");
-        assertTrue(Palindrome.isPalindromeNumber(0),"0 is a palindrome");
-
+        assertTrue(Palindrome.isPalindromeNumber(44), "44 is a palindrome");
+        assertTrue(Palindrome.isPalindromeNumber(0), "0 is a palindrome");
     }
 
     @Test
     void testNonPalindromes() {
-
-        assertFalse(Palindrome.isPalindromeNumber(1255),"123 is not  a palindrome");
-        assertFalse(Palindrome.isPalindromeNumber(10),"10 is not a palindrome");
-        assertFalse(Palindrome.isPalindromeNumber(100),"100 is not a palindrome");
-
+        assertFalse(Palindrome.isPalindromeNumber(1255), "123 is not  a palindrome");
+        assertFalse(Palindrome.isPalindromeNumber(10), "10 is not a palindrome");
+        assertFalse(Palindrome.isPalindromeNumber(100), "100 is not a palindrome");
     }
 
     @Test
     void testNegativeNumbers() {
-        assertFalse(Palindrome.isPalindromeNumber(-121),"-121 is not a palindrome");
-        assertFalse(Palindrome.isPalindromeNumber(-1),"-1 is not a palindrome" );
-        assertFalse(Palindrome.isPalindromeNumber(-101),"-101 is not a palindrome");
+        assertFalse(Palindrome.isPalindromeNumber(-121), "-121 is not a palindrome");
+        assertFalse(Palindrome.isPalindromeNumber(-1), "-1 is not a palindrome");
+        assertFalse(Palindrome.isPalindromeNumber(-101), "-101 is not a palindrome");
     }
 
     @Test
     void testLargeNumbers() {
-        assertTrue(Palindrome.isPalindromeNumber(123454321),"123454321 is a palindrome");
+        assertTrue(Palindrome.isPalindromeNumber(123454321), "123454321 is a palindrome");
     }
-
 }
---
+++ lab3/README.md
@@ -0,0 +1,12 @@
+# Lab 3 home work solutions
+
+- This folder contains solutions to lab 3
+
+| Question | File with solution                     |
+| -------- | -------------------------------------- |
+| 1        | Exponential.java                       |
+| 2        | MinChar.java                           |
+| 3        | ArrayReversal.java                     |
+| 4        | Palindrome.java                        |
+| 5        | MaxValue.java                          |
+| 6        | PalindromeTest.java, MaxValueTest.java |
