79817d817be735147797ed73a5d460075743f1dd
kizzanaome <naomekizzan@gmail.com>
2025-03-12 16:36:33 -0500

    Add solutions to lab 6

--- lab6/problem2/Statistics.java
+++ lab6/problem2/Statistics.java
@@ -1,36 +1,33 @@
 package lab6.problem2;
 
 import java.util.List;
-abstract public class Statistics implements EmployeeData{
-	/** 
-	 * Polymorphically computes and returns the sum
-	 * of all the salaries of all the staff/teachers in the list.
-	 */
 
-	public static double computeSumOfSalaries(List<EmployeeData> aList) {
+abstract public class Statistics implements EmployeeData {
+    /**
+     * Polymorphically computes and returns the sum
+     * of all the salaries of all the staff/teachers in the list.
+     */
 
-		//implement
-		//compute sum of all salaries of people in aList and 
-		//loop through the alist 
-		if(aList == null){
-			return 0.0;
-		}
+    public static double computeSumOfSalaries(List<EmployeeData> aList) {
 
-		double sum = 0.0;
-		
-		//loop through the combined list
+        //implement
+        //compute sum of all salaries of people in aList and
+        //loop through the alist
+        if (aList == null) {
+            return 0.0;
+        }
+
+        double sum = 0.0;
+
+        //loop through the combined list
         for (EmployeeData employee : aList) {
-			if(employee !=null){
-				sum += employee.getSalary();
-			}
-            
+            if (employee != null) {
+                sum += employee.getSalary();
+            }
+
         }
         return sum;
 
 
-		
-	
-
-		
-	}
+    }
 }
--- lab6/problem4/DoublyLinkedList.java
+++ lab6/problem4/DoublyLinkedList.java
@@ -1,16 +1,17 @@
 package lab6.problem4;
 
 public class DoublyLinkedList {
-	
-		Node header;
-		DoublyLinkedList(){
-			header = new Node();
-		}
 
-		//1. adds to the end of the list
-		public void addLast(String item){
-			//implement
-		Node newNode = new Node(item);
+    Node header;
+
+    DoublyLinkedList() {
+        header = new Node();
+    }
+
+    //1. adds to the end of the list
+    public void addLast(String item) {
+        //implement
+        Node newNode = new Node(item);
         Node current = header;
         while (current.next != null) {
             current = current.next;
@@ -19,12 +20,11 @@ public class DoublyLinkedList {
         newNode.previous = current;
     }
 
-			
-		
-		// 2. Remove by passing object
-		public boolean remove(String item){
-			//Implement
-		Node current = header.next;
+
+    // 2. Remove by passing object
+    public boolean remove(String item) {
+        //Implement
+        Node current = header.next;
         while (current != null) {
             if (current.value.equals(item)) {
                 if (current.next != null) {
@@ -38,13 +38,13 @@ public class DoublyLinkedList {
             current = current.next;
         }
         return false;
-		}
+    }
 
-		// 3. Remove the First Node
+    // 3. Remove the First Node
 
-		public boolean removeFirst() {
-		  // Implement
-		    if (header.next == null) {
+    public boolean removeFirst() {
+        // Implement
+        if (header.next == null) {
             return false;
         }
         header.next = header.next.next;
@@ -52,13 +52,13 @@ public class DoublyLinkedList {
             header.next.previous = header;
         }
         return true;
-		}
+    }
 
 
-		// 4. Prints the list from last to first
-		 public void printReverse() {
-		     // Implement
-			 Node current = header;
+    // 4. Prints the list from last to first
+    public void printReverse() {
+        // Implement
+        Node current = header;
         while (current.next != null) {
             current = current.next;
         }
@@ -67,47 +67,49 @@ public class DoublyLinkedList {
             current = current.previous;
         }
         System.out.println();
-		}
-		@Override
-		public String toString() {
-			StringBuilder sb = new StringBuilder();
-			toString(sb, header);
-			return sb.toString();
-			
-		}
-		
-		private void toString(StringBuilder sb, Node n) {
-			   if (n == null) return;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        toString(sb, header);
+        return sb.toString();
+
+    }
+
+    private void toString(StringBuilder sb, Node n) {
+        if (n == null) return;
         sb.append(n.value).append(" ");
         toString(sb, n.next);
-		}
-		
-		class Node {
-			String value;
-			Node next;
-			Node previous;
-			
-   @Override
-			public String toString() {
-				return value == null ? "null" : value;
-			}
+    }
 
-		 Node() {}
+    class Node {
+        String value;
+        Node next;
+        Node previous;
+
+        @Override
+        public String toString() {
+            return value == null ? "null" : value;
+        }
+
+        Node() {
+        }
 
         Node(String value) {
             this.value = value;
         }
-		}
-	
-		public static void main(String[] args){
-			DoublyLinkedList list = new DoublyLinkedList();
-			list.addLast("Bob");
-			list.addLast("Harry");
-			list.addLast("Steve");
-			System.out.println(list);
+    }
 
-			// Call all your implemented Methods 
-			list.addLast("Carol");
+    public static void main(String[] args) {
+        DoublyLinkedList list = new DoublyLinkedList();
+        list.addLast("Bob");
+        list.addLast("Harry");
+        list.addLast("Steve");
+        System.out.println(list);
+
+        // Call all your implemented Methods
+        list.addLast("Carol");
         System.out.println(list); // Expected: Bob Bill Tom Carol
 
         list.remove("Bill");
@@ -117,5 +119,5 @@ public class DoublyLinkedList {
         System.out.println(list); // Expected: Tom Carol
 
         list.printReverse(); // Expected: Carol Tom
-		}
+    }
 }
