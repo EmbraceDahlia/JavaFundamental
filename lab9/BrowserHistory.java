5906fe579e84881c801cf74c5bf86bc03364334f
Gideon Bamuleseyo <gideonbamuleseyo@gmail.com>
2025-03-13 15:55:01 -0500

    Add lab 9 solutions

---
+++ lab9/BrowserHistory.java
@@ -0,0 +1,68 @@
+package lab9;
+
+import java.util.Stack;
+
+public class BrowserHistory {
+
+    private String currentUrl;
+    Stack<String> history = new Stack<>();
+    Stack<String> forwardUrls = new Stack<>();
+
+    public BrowserHistory(String currentUrl) {
+        this.currentUrl = currentUrl;
+    }
+
+    public String getCurrentUrl() {
+        return currentUrl;
+    }
+
+    public void setCurrentUrl(String currentUrl) {
+        this.currentUrl = currentUrl;
+    }
+
+    public void visit(String url) {
+        if (url == null || url.isEmpty())
+            throw new IllegalArgumentException("Invalid url");
+        history.push(this.currentUrl);
+        this.currentUrl = url;
+        forwardUrls.clear();
+        System.out.println("Visited: " + url);
+    }
+
+    public void back() {
+        if (history.size() == 0) {
+            System.out.println("No history to go back to.");
+            return;
+        }
+        String previousUrl = history.pop();
+        System.out.println("Back to: " + previousUrl);
+        forwardUrls.push(this.currentUrl);
+        this.currentUrl = previousUrl;
+    }
+
+    public void forward() {
+        if (forwardUrls.size() == 0) {
+            System.out.println("No forward history. ");
+            return;
+        }
+        String forwardUrl = forwardUrls.pop();
+        this.currentUrl = forwardUrl;
+        System.out.println("Forward to: " + forwardUrl);
+
+    }
+
+    public static void main(String[] args) {
+        BrowserHistory browser = new BrowserHistory("home.html");
+        // Start with home.html
+        browser.visit("page1.html");
+        browser.visit("page2.html");
+        browser.back();
+        browser.back();
+        browser.back();
+        browser.forward();
+        browser.forward();
+        browser.forward();
+        browser.visit("page3.html");
+    }
+
+}
---
+++ lab9/LinkedListStack.java
@@ -0,0 +1,64 @@
+package lab9;
+
+import java.util.LinkedList;
+
+public class LinkedListStack {
+
+    LinkedList<Integer> list = new LinkedList<>();
+
+    public void push(Integer x) {
+        if (x == null)
+            return;
+        list.addLast(x);
+    }
+
+    public Integer peek() {
+        // returns null if stack is empty
+        if (list.isEmpty()) {
+            return null;
+        }
+        return list.getLast();
+    }
+
+    public Integer pop() {
+
+        if (list.isEmpty()) {
+            return null;
+        }
+        return list.removeLast();
+    }
+
+    public boolean isEmpty() { // true if stack is empty
+        return list.isEmpty();
+    }
+
+    public int size() { // returns number of items in the stack
+        return list.size();
+    }
+
+    @Override
+    public String toString() {
+        if (list.isEmpty())
+            return "<empty>";
+        StringBuilder sb = new StringBuilder();
+        for (Integer x : list) {
+            if (x != null)
+                sb.append(x.toString()).append(" ");
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        LinkedListStack listStack = new LinkedListStack();
+        listStack.push(1);
+        listStack.push(2);
+        System.out.println(listStack);
+        System.out.println("Size = " + listStack.size());
+        System.out.println(listStack.isEmpty());
+        listStack.pop();
+        System.out.println(listStack);
+        listStack.pop();
+        System.out.println(listStack);
+    }
+
+}
