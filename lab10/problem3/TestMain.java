5913d8f2542727941068af022619703156e79197
kizzanaome <naomekizzan@gmail.com>
2025-03-14 14:57:04 -0500

    Add lab10 problem3 solutions

---
+++ lab10/problem3/TestMain.java
@@ -0,0 +1,10 @@
+package lab10.problem3;
+
+public class TestMain {
+
+    public static void main(String[] args) {
+        Library lib = new Library();
+        lib.addBook("111", "Greatnness", "Me");
+
+    }
+}
