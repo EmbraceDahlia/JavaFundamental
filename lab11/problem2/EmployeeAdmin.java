b7ce9277754df4e1327617845d64f56d00cf723b
Dahlia <embracedahlia@gmail.com>
2025-03-15 14:57:53 -0500

    Add exception handling in employee report

--- lab11/problem2/EmployeeAdmin.java
+++ lab11/problem2/EmployeeAdmin.java
@@ -8,19 +8,21 @@ import java.util.Collections;
 public class EmployeeAdmin {
 
     /**
-
-     Your method prepareReport should return a list of all Employees in the input table
-     whose social security number is in the input list socSecNums and whose salary is greater than $80,000.
-     In addition, this list of Employees must be sorted by social security number, in ascending order (from numerically smallest to numerically largest).
+     * Your method prepareReport should return a list of all Employees in the input table
+     * whose social security number is in the input list socSecNums and whose salary is greater than $80,000.
+     * In addition, this list of Employees must be sorted by social security number, in ascending order (from numerically smallest to numerically largest).
      */
     public static List<Employee> prepareReport(HashMap<String, Employee> table, List<String> socSecNums) {
-        List<Employee> list=new ArrayList<>();
-        table.forEach((k,v)->{
-            if(socSecNums.contains(v.getSsn()) && v.getSalary()>80000)
+        if (table == null) throw new IllegalArgumentException("Employee table cannot be null!");
+        if (socSecNums == null) throw new IllegalArgumentException("List of social security number cannot be null!");
+
+        List<Employee> list = new ArrayList<>();
+        table.forEach((k, v) -> {
+            if (socSecNums.contains(v.getSsn()) && v.getSalary() > 80000)
                 list.add(v);
         });
-        Collections.sort(list,(c1,c2)->
-            c1.getSsn().compareTo(c2.getSsn())
+        Collections.sort(list, (c1, c2) ->
+                c1.getSsn().compareTo(c2.getSsn())
         );
         return list;
     }
